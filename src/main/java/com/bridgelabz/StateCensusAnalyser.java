a98c922b2b95266b99f892311b8262548f38a354
shravkv <sravan.k.vodnala@gmail.com>
2022-07-14 21:55:41 +0530

    Added: welcome to state census analyser

---
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class StateCensusAnalyser {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome Indian Census Analyser Program");
+    }
+}
